19f2ff4514f2b00eb1a78d3dc95938b2ca2ddad0
sakthi <sakthivignesh.s@hyperready.tech>
2025-05-08 00:57:11 +0530

    db changes

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,10 +1,6 @@
 import mongoose from 'mongoose';
 
-const MONGODB_URI = "mongodb+srv://VantaVerseSakthiSvsp:WfeDMZ$CYkThZm2@vantaverselovegame.cjjckqh.mongodb.net/askitai?retryWrites=true&w=majority"
-
-if (!MONGODB_URI) {
-  throw new Error('Please define the MONGODB_URI environment variable');
-}
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://VantaVerseSakthiSvsp:WfeDMZ$CYkThZm2@vantaverselovegame.cjjckqh.mongodb.net/askitai?retryWrites=true&w=majority';
 
 const connectDB = async () => {
   try {
@@ -12,7 +8,10 @@ const connectDB = async () => {
       return;
     }
 
-    await mongoose.connect(MONGODB_URI);
+    await mongoose.connect(MONGODB_URI, {
+      // Remove any Kerberos-related options
+      authSource: 'admin',
+    });
 
     console.log('MongoDB connected successfully');
   } catch (error) {
